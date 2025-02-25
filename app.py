7e76b52bdcbd54aaa24bf75fff99348bf52a6a86
Vincent Nguyen <vincent.nguyen.travail@gmail.com>
2025-02-25 17:08:31 +0100

    add theme selection

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
+st.title("SQL spaced repetition")
+
+option = st.selectbox(
+    "What would you like to review ?", 
+    ["Group By", "Window functions"], 
+    index=None
+)
+
 df = pd.DataFrame({
     'a': [1, 2, 3],
     'b': [4, 5, 6]}
