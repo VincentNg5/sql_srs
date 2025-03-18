de0cf1d0c4b71655ef85fddc15e55ed5162f1724
Vincent Nguyen <vincent.nguyen.travail@gmail.com>
2025-03-18 16:33:04 +0100

    refactor code to compare solution with answer

--- app.py
+++ app.py
@@ -28,33 +28,38 @@ CROSS JOIN food_items
 
 sql_query = st.text_area("SQL", "SELECT * FROM beverages")
 if sql_query:
-    result = con.execute(sql_query).df()
-    st.dataframe(result)
+    result_df = con.execute(sql_query).df()
+    st.dataframe(result_df)
 
-    # if len(result.columns) != len(solution.columns):
-    #     st.write("Some columns are missing")
+    exercise_name = exercise_df.loc[0, "exercise_name"]
+    solution_fname = f"answers/{exercise_name}.sql"
+    with open(solution_fname, "r") as f:
+        solution = f.read()
+    
+    solution_df = con.execute(solution).df()
 
-    # try:
-    #     result = result[solution.columns]
-    #     st.dataframe(result.compare(solution))
-    # except KeyError as e:
-    #     st.write("Some columns are missing")
+    if len(result_df.columns) != len(solution_df.columns):
+        st.write("Some columns are missing")
 
-    # n_lines_difference = len(result) - len(solution)
-    # if n_lines_difference != 0:
-    #     st.write(f"Number of lines is different by {n_lines_difference}")
+    try:
+        result_df = result_df[solution_df.columns]
+        df_comparison = result_df.compare(solution_df)
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = len(result_df) - len(solution_df)
+    if n_lines_difference != 0:
+        st.write(f"Number of lines is different by {n_lines_difference}")
+    
 
 tab_tables, tab_solution = st.tabs(["Tables", "Solution"])
 with tab_tables:
     exercice_tables = exercise_df.loc[0, "tables"]
     for table in exercice_tables:
         st.write(f"table: {table}")
-        df_table = con.execute(f"SELECT * FROM {table}").df()
-        st.dataframe(df_table)
+        table_df = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(table_df)
 
 with tab_solution:
-    exercise_name = exercise_df.loc[0, "exercise_name"]
-    solution_fname = f"answers/{exercise_name}.sql"
-    with open(solution_fname, "r") as f:
-        solution = f.read()
     st.write(solution)
+    
